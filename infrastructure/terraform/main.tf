ad082001e15758f990cff3ac29032905ee01ebca
vivekchalla246 <vivekchalla246@gmail.com>
2026-03-21 19:49:51 +0000

    updated security groups for jenkins

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -35,7 +35,21 @@ module "jenkins_sg" {
       to_port     = 8080
       protocol    = "tcp"
       cidr_blocks = [var.my_ip]
+    },
+    {
+      from_port   = 80
+      to_port     = 80
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    },
+
+    {
+      from_port   = 443
+      to_port     = 443
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
     }
+
   ]
 
   egress_rules = [
